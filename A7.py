2f6a367e96edb32623b9cdd4f4a250d77f96df5a
Adusumalli Sruthi <170030015@kluniversity.in>
2022-09-01 10:01:56 +0530

    Commit Done on 19-08-2022

---
+++ A7.py
@@ -0,0 +1,9 @@
+class practice:
+    name
+    id
+    def display(self):
+        self.name=input("enter name")
+        self.id=input("enter id number")
+        print(self.name,self.id)
+ob=practice()
+ob.display()
